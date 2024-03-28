f84fdff24438844d2262f2edbc1dd05529887fa4
Elisa Petit-Sirigu <E.Petit.inf21@stud.bbbaden.ch>
2024-03-28 10:29:09 +0100

    installed and used react-native-safe-area-context

--- budgetCalculator/App.js
+++ budgetCalculator/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+function HomeScreen() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ flex: 1, paddingTop: insets.top }}>
+      <Text style={{ fontSize: 28 }}>Content is in safe area.</Text>
+    </View>
+  );
+}
+//<SafeAreaProvider> can be applied as padding or margin 
+//<useSafeAreaInsets> is used as a hook
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <HomeScreen/>
+    </SafeAreaProvider>
   );
 }
 
